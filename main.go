dbdf8aa80081850b32e79965bd6f54edbe1a2fe2
FromSi <fromsi665@gmail.com>
2024-03-13 23:49:31 +0500

    init code

--- main.go
+++ main.go
@@ -1,16 +1,115 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+  "github.com/gin-gonic/gin"
+  "github.com/go-playground/validator/v10"
+)
 
-func main() {
-	r := gin.Default()
-
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello World!",
-		})
-	})
-
-	r.Run() // listen and serve on 0.0.0.0:8080
+type ErrorItemResponse struct {
+  Field string `json:"field"`
+  Tag   string `json:"tag"`
 }
 
+type IErrorResponse interface {
+  From(err validator.ValidationErrors) []ErrorItemResponse
+}
+
+type ErrorResponse struct {}
+
+func (e ErrorResponse) From(err error) []ErrorItemResponse {
+  var result []ErrorItemResponse
+
+  for _, err := range err.(validator.ValidationErrors) {
+    item := ErrorItemResponse{Field: err.Field(), Tag: err.Tag()}
+
+    result = append(result, item)
+  }
+
+  return result
+}
+
+type ChatIdRequest struct {
+  Id string `uri:"id" binding:"required,number"`
+}
+
+type ChatIdResponse struct {
+  Id        int                     `json:"id"`
+  Username  string                  `json:"username"`
+  UserId    int                     `json:"user_id"`
+  Messages  []ChatIdMessageResponse `json:"messages"`
+}
+
+type ChatIdMessageResponse struct {
+  Id      int     `json:"id"`
+  ChatId  int     `json:"chat_id"`
+  UserId  int     `json:"user_id"`
+  Text    string  `json:"text"`
+}
+
+type ChatResponse struct {
+	Id          int     `json:"id"`
+	Username    string  `json:"username"`
+	Avatar      string  `json:"avatar"`
+	LastMessage string  `json:"last_message"`
+}
+
+type BaseResponse struct {
+  Data interface{} `json:"data"`
+}
+
+func main() {
+	route := gin.Default()
+
+	route.GET("/chats", func(c *gin.Context) {
+    data := []ChatResponse{
+      ChatResponse{
+        Id:          1,
+        Username:    "test1",
+        Avatar:      "test1",
+        LastMessage: "test1",
+      },
+      ChatResponse{
+        Id:          2,
+        Username:    "test2",
+        Avatar:      "test2",
+        LastMessage: "test2",
+      },
+    }
+
+		c.JSON(200, BaseResponse{Data: data})
+	})
+
+	route.GET("/chats/:id", func(c *gin.Context) {
+    var req ChatIdRequest
+
+    if err := c.ShouldBindUri(&req); err != nil {
+      c.JSON(200, BaseResponse{Data: ErrorResponse{}.From(err)})
+
+      return
+    }
+
+	  data := ChatIdResponse{
+      Id:        1,
+      Username:  "test",
+      UserId:    1,
+      Messages:  []ChatIdMessageResponse{
+        ChatIdMessageResponse{
+          Id:      1,
+          ChatId:  1,
+          UserId:  1,
+          Text:    "test",
+        },
+        ChatIdMessageResponse{
+          Id:      2,
+          ChatId:  1,
+          UserId:  1,
+          Text:    "test2",
+        },
+      },
+	  }
+
+		c.JSON(200, BaseResponse{Data: data})
+	})
+
+	route.Run()
+}
